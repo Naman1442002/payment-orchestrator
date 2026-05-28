aa110acbda7dbcc843ea7b9b2b78e181f62be4fa
NamanPal <naman.pal@innovationm.com>
2026-05-28 19:50:26 +0530

    Move HealthController from config to controller

--- backend/payment-system/src/main/java/com/naman/paymentsystem/controller/HealthController.java
+++ backend/payment-system/src/main/java/com/naman/paymentsystem/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.naman.paymentsystem.config;
+package com.naman.paymentsystem.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
